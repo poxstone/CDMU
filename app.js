43af1a88156316d00c722c75970eb0a5f7ac429e
poxstone <poxstone@gmail.com>
2015-11-03 23:11:27 -0500

    adicion iframe

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var mongoose = require('mongoose');
+//var mongoose = require('//mongoose');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var api = require('./routes/api');
+//var api = require('./routes/api');
 
 // Connect to the beerlocker MongoDB
-mongoose.connect('mongodb://localhost:27017/beerlocker');
+//mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api', api);
+//app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
